f1a61c345b0b5a16ac0cb79b0b39c0646554d2ba
kanakkhandelwal25~ <kkhandelwal1590@gmail.com>
2023-04-03 23:33:19 +0530

    Episode 3 started

--- App.js
+++ App.js
@@ -3,6 +3,8 @@
 //what is CDN
 //crossorigin attribute in script tag
 //React and ReactDOM
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
 const heading = React.createElement(
     'h1', 
@@ -22,18 +24,38 @@ const parentElement = React.createElement(
             React.createElement(
                 'h1',
                 {},
-                'Nested components in react'
+                'Nested components in React'
             ),
             React.createElement(
                 'h2',
-                {},
+                {}, 
                 'h2 Component'
+            ),
+            React.createElement(
+                'p',
+                {id:'para'},
+                'This is a paragraph'
             )
         ]
     )
 )
 console.log(parentElement);
 
+const jsxHeading = (
+    <h1 id='heading' className='head'>
+        This is JSX
+    </h1>
+);
+
+console.log(jsxHeading);
+console.log(heading);
+
+
+//React Components
+const headingComponent = ()  => {
+    return <h1>React Functional Component</h1>;
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(parentElement);
+root.render(jsxHeading);
 
